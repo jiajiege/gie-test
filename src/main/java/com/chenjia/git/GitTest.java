e8427dfa11aa9dc03796be744af672f3ed5c5d54
ChJ <ChJ@chenjia.com>
2022-04-06 17:46:38 +0800

    third commit

--- src/main/java/com/chenjia/git/GitTest.java
+++ src/main/java/com/chenjia/git/GitTest.java
@@ -7,5 +7,8 @@ package com.chenjia.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+
     }
 }
