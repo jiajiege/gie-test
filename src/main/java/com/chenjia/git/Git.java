89607832bdd4b5dfe20e9fcd173cbf36cac2e4e1
ChJ <ChJ@chenjia.com>
2022-04-06 17:20:03 +0800

    first commit

---
+++ src/main/java/com/chenjia/git/Git.java
@@ -0,0 +1,13 @@
+package com.chenjia.git;
+
+import sun.applet.Main;
+
+/**
+ * @author chenjia
+ * @create 2022-04-06 17:14
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
---
+++ src/main/java/com/chenjia/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.chenjia.git;
+
+/**
+ * @author chenjia
+ * @create 2022-04-06 16:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
